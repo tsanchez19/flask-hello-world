849c26f5e1de56856f04c53a5304dbb7aac4bd49
tsanchez19 <tysa5330@colorado.edu>
2023-04-04 04:11:17 +0000

    db_insert

--- app.py
+++ app.py
@@ -45,32 +45,3 @@ def inserting():
     conn.close()
     return "Basketball Table Successfully Populated"
 
-@app.route('/db_select')
-def selecting():
-    conn = psycopg2.connect("postgres://lab10tsdb_user:6agfPU8lS9HEoX1PefNWXxgdkREINjJc@dpg-cglpdthmbg56g47s7920-a/lab10tsdb")
-    cur = conn.cursor()
-    cur.execute('''
-        SELECT * FROM Basketball
-    ''')
-    records = cur.fetchall()
-    conn.close()
-    response_string = ""
-    response_string += "<table>"
-    for player in records:
-        response_string += "<tr>"
-        for info in player:
-            response_string += "<td>{}<td>".format(info)
-        response_string += "<tr>"
-    response_string += "<table>"
-    return response_string
-
-@app.route('/db_drop')
-def dropping():
-    conn = psycopg2.connect("postgres://lab10tsdb_user:6agfPU8lS9HEoX1PefNWXxgdkREINjJc@dpg-cglpdthmbg56g47s7920-a/lab10tsdb")
-    cur = conn.cursor()
-    cur.execute('''
-        DROP TABLE Basketball;
-    ''')
-    conn.commit()
-    conn.close()
-    return "Basketball Table Successfully Dropped"
